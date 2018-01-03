74a438c030873d5b1919adfb602e8982738d23ba
BARRY POWER <BARRY.POWER@LIBERTY-IT.CO.UK>
2018-01-03 09:08:56 +0000

    update

--- branch-manager.rb
+++ branch-manager.rb
@@ -1,7 +1,7 @@
 class BranchManager < Formula
   desc "Manage branches for git repos across subdirectories"
   homepage "https://github.com/barryjpower/homebrew-branch-manager"
-  url "https://github.com/barryjpower/homebrew-branch-manager/archive/0.0.4.tar.gz"
+  url "https://github.com/barryjpower/homebrew-branch-manager/archive/0.0.5.tar.gz"
   sha256 "701990ef5b6262b113334ea7610caa164caf7a4caba7fd19335e3d73ff7eff7f"
 
   def install
